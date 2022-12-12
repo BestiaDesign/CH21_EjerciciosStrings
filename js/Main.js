e4a492d7bc8298b35bdaf7d30406ca1cc5f6077a
Johanna <bestiadesign1@gmail.com>
2022-12-12 14:36:17 -0600

    Se actualizaron el html y el js del ejercicio 1

--- js/Main.js
+++ js/Main.js
@@ -1,14 +1,9 @@
-
-let nombre = "Johanna"; // "Johanna";
+// Ejercicio 1
+// Solución 1
+/* let nombre = "Johanna"; // "Johanna";
 nombre = nombre.toLowerCase();
-
-/* let nombreMostrado = document.getElementById("nombreMostrado");
-let alertResultado = document.getElementById("alertResultado"); */
 let letra = "";
 let contFinal = 0;
-
-/* nombreMostrado.addEventListener("click", function (event){
-    event.preventDefault(); */
 
 for (let index = 0; index < nombre.length; index++) {
     console.log(nombre.charAt(index));
@@ -24,4 +19,47 @@ for (let index = 0; index < nombre.length; index++) {
     } // contTemp>contFinal
 } // for index
 //});
-console.log(letra, contFinal);
+console.log(letra, contFinal); */
+
+// Solución 2
+
+let nombreMostrado = document.getElementById("nombreMostrado");
+let alertResultado = document.getElementById("alertResultado");
+
+nombreMostrado.addEventListener("click", function (event){
+    event.preventDefault();
+
+    let nombre = document.getElementById("inputNombre").value;
+    nombre = nombre.toLowerCase();
+    let letra = "";
+let contFinal = 0;
+
+for (let index = 0; index < nombre.length; index++) {
+    // console.log(nombre.charAt(index));
+    let contTemp = 1;
+    for (let contador = index+1; contador < nombre.length; contador++) {
+        if(nombre.charAt(index) == nombre.charAt(contador))
+        contTemp++;
+    } // for contador
+
+    if (contTemp > contFinal) {
+        contFinal=contTemp;
+        letra = nombre.charAt(index);
+    } // contTemp>contFinal
+} // for index
+alertResultado.innerHTML = 'La letra que más se repite en ' + nombre + ' es: ' + letra;  
+});
+// console.log(letra, contFinal);
+
+
+// Ejercicio 2
+
+for (let index = nombre.length; index++) {
+    const element = array[index];
+    
+}
+
+// Entrada
+"Nancy Johanna"
+// Salida
+"annahoJ cynaN"
