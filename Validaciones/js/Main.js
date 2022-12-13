d488e776632efb918c240c1dcb058cda4df24581
Johanna <bestiadesign1@gmail.com>
2022-12-13 11:18:16 -0600

    Se modificaron lor archivos de email y RFC

--- Validaciones/js/Main.js
+++ Validaciones/js/Main.js
@@ -1,4 +1,4 @@
-// Aquí se escriben los ejercicios con Strings
+ /*// Aquí se escriben los ejercicios con Strings
 
 let btnEnviar = document.getElementById("btnEnviar");
 btnEnviar = document.addEventListener("click", function(event) {
@@ -12,24 +12,50 @@ btnEnviar = document.addEventListener("click", function(event) {
     alertError.style.display = "none";
     alertError.innerHTML="";
 
-    /* console.log(exampleFormControlTextTarea1.value.length);
-    console.log("[" +
-    exampleFormControlTextTarea1.value.replaceAll(" ", "") + "]"
-    ); */
+    // console.log(exampleFormControlTextTarea1.value.length);
+    // console.log("[" +
+    // exampleFormControlTextTarea1.value.replaceAll(" ", "") + "]"
+    // ); 
 
-    if (exampleFormControlTextTarea1.value.trim().replaceAll(" ", "").length < 20) {
+    /*if (exampleFormControlTextTarea1.value.trim().replaceAll(" ", "").length < 20) {
         alertError.innerHTML = "El mensaje debe contener 20 caracteres o más";
         alertError.style.display = "block";
         exampleFormControlTextTarea1.focus();
         exampleFormControlTextTarea1.select();
-    }
+    } */
 // if
 
 // Match
 // Expresiones regulares
-
+/*
 if (exampleFormControlInput1.value.match(emailRegex) == null) {
     alertError.style.display = "block";
     alertError.innerHTML += "<br/>El correo electrónico no es válido.";
+    }
+}); */
+
+//Aquí se escribiran los ejercicios con Strings
+let btnEnviar = document.getElementById("btnEnviar");
+btnEnviar.addEventListener("click", function(event){
+    event.preventDefault();
+   
+    let emailRegex =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g; //https://emailregex.com/
+
+    let exampleFormControlInput1 = document.getElementById("exampleFormControlInput1"); //email
+    let exampleFormControlTextTarea1 = document.getElementById("exampleFormControlTextTarea1");
+    let alertError = document.getElementById("alertError");
+    exampleFormControlTextTarea1.value = exampleFormControlTextTarea1.value.trim();
+    alertError.style.display="none";
+    alertError.innerHTML="";
+    if (exampleFormControlTextTarea1.value.trim().replaceAll("  " , "").length < 20){
+        alertError.innerHTML = "El mensaje debe contener 20 caracteres o más.";
+        alertError.style.display="block";
+        exampleFormControlTextTarea1.focus();
+        exampleFormControlTextTarea1.select();
+    }//if
+
+    if (exampleFormControlInput1.value.match(emailRegex) == null) {
+        alertError.style.display="block";
+        alertError.innerHTML += "<br/>El correo electrónico no es válido.";
     }
 });
--- Validaciones/js/Main_RFC.js
+++ Validaciones/js/Main_RFC.js
@@ -1,35 +1,33 @@
-// Aquí se escriben los ejercicios con Strings
-
 let btnEnviar = document.getElementById("btnEnviar");
-btnEnviar = document.addEventListener("click", function(event) {
+btnEnviar.addEventListener("click", function(event){
     event.preventDefault();
-    let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-    let exampleFormControlInput1 = document.getElementById("exampleFormControlInput1");
-    let exampleFormControlTextTarea1 = document.getElementById("exampleFormControlTextTarea1");
+    let emailRegex =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g; //https://emailregex.com/
+
+    let exampleFormControlInput1 = document.getElementById("exampleFormControlInput1"); //email
+    let exampleFormControlTextTarea1 = document.getElementById("exampleFormControlTextTarea1"); // RFC
     let alertError = document.getElementById("alertError");
     exampleFormControlTextTarea1.value = exampleFormControlTextTarea1.value.trim();
-    alertError.style.display = "none";
+    alertError.style.display="none";
     alertError.innerHTML="";
-
-    /* console.log(exampleFormControlTextTarea1.value.length);
-    console.log("[" +
-    exampleFormControlTextTarea1.value.replaceAll(" ", "") + "]"
-    ); */
-
-    if (exampleFormControlTextTarea1.value.trim().replaceAll(" ", "").length < 20) {
-        alertError.innerHTML = "El mensaje debe contener 20 caracteres o más";
-        alertError.style.display = "block";
+    if (exampleFormControlTextTarea1.value.trim().replaceAll("  " , "").length < 20){
+        alertError.innerHTML = "El mensaje debe contener 20 caracteres o más.";
+        alertError.style.display="block";
         exampleFormControlTextTarea1.focus();
         exampleFormControlTextTarea1.select();
-    }
-// if
+    }//if
 
-// Match
-// Expresiones regulares
-
-if (exampleFormControlInput1.value.match(emailRegex) == null) {
-    alertError.style.display = "block";
-    alertError.innerHTML += "<br/>El correo electrónico no es válido.";
+    if (exampleFormControlInput1.value.match(emailRegex) == null) {
+        alertError.style.display="block";
+        alertError.innerHTML += "<br/>El correo electrónico no es válido.";
     }
+let RFCRegex = /^ [A-Z]{4}[0-9]{6}[A-Z0-9]{3}$/;
+// /^ [A-Z]{4}\d{6}\w{3}$/;
+exampleFormControlInput2.value = exampleFormControlInput2.value.toUpperCase();
+    if (exampleFormControlInput2.value.match(RFCRegex) == null) {
+        alertError.style.display = "block";
+        alertError.innerHTML += "<br/>El RFC no es válido.";
+        }
+
+
 });
