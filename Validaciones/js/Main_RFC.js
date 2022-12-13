37301f5f183eb598cc8ad34015f040175a26e7b5
Johanna <bestiadesign1@gmail.com>
2022-12-13 11:59:05 -0600

    se agregó Timeout en archivo js

--- Validaciones/js/Main_RFC.js
+++ Validaciones/js/Main_RFC.js
@@ -1,11 +1,12 @@
-let btnEnviar = document.getElementById("btnEnviar");
+/* let btnEnviar = document.getElementById("btnEnviar");
 btnEnviar.addEventListener("click", function(event){
     event.preventDefault();
 
     let emailRegex =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g; //https://emailregex.com/
 
     let exampleFormControlInput1 = document.getElementById("exampleFormControlInput1"); //email
-    let exampleFormControlTextTarea1 = document.getElementById("exampleFormControlTextTarea1"); // RFC
+    let exampleFormControlTextTarea1 = document.getElementById("exampleFormControlTextTarea1"); // Mensaje
+    let exampleFormControlInput2 = document.getElementById("exampleFormControlInput2"); // RFC
     let alertError = document.getElementById("alertError");
     exampleFormControlTextTarea1.value = exampleFormControlTextTarea1.value.trim();
     alertError.style.display="none";
@@ -20,14 +21,64 @@ btnEnviar.addEventListener("click", function(event){
     if (exampleFormControlInput1.value.match(emailRegex) == null) {
         alertError.style.display="block";
         alertError.innerHTML += "<br/>El correo electrónico no es válido.";
-    }
-let RFCRegex = /^ [A-Z]{4}[0-9]{6}[A-Z0-9]{3}$/;
-// /^ [A-Z]{4}\d{6}\w{3}$/;
-exampleFormControlInput2.value = exampleFormControlInput2.value.toUpperCase();
+    } */
+
+    //Aquí se escribiran los ejercicios con Strings
+
+let btnEnviar = document.getElementById("btnEnviar");
+let idTimeout;
+
+    btnEnviar.addEventListener("click", function(event){
+    event.preventDefault();
+
+    let emailRegex =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;  //https://emailregex.com/
+
+    let exampleFormControlInput1 = document.getElementById("exampleFormControlInput1"); //email
+    let exampleFormControlInput2 = document.getElementById("exampleFormControlInput2");  //RFC
+    let exampleFormControlTextTarea1 = document.getElementById("exampleFormControlTextTarea1"); // Mensaje
+    let alertError = document.getElementById("alertError");
+    exampleFormControlTextTarea1.value = exampleFormControlTextTarea1.value.trim();
+    alertError.style.display="none";
+    alertError.innerHTML="";
+    if (exampleFormControlTextTarea1.value.trim().replaceAll("  " , "").length < 20){
+        alertError.innerHTML = "El mensaje debe contener 20 caracteres o más.";
+        alertError.style.display="block";
+        exampleFormControlTextTarea1.focus();
+        exampleFormControlTextTarea1.select();
+        exampleFormControlTextTarea1.style.border = "solid red 1px";
+    }else {
+        exampleFormControlTextTarea1.style.border = "";
+    }//if
+
+    if (exampleFormControlInput1.value.match(emailRegex) == null) {
+        alertError.style.display="block";
+        alertError.innerHTML += "<br/>El correo electrónico no es válido.";
+        exampleFormControlInput1.style.border = "solid red 1px";
+    }else {
+            exampleFormControlInput1.style.border = "";
+    }//
+    
+    let RFCRegex = /^[A-Z]{4}[0-9]{6}[A-Z0-9]{3}$/;
+    //  /^[A-Z]{4}\d{6}\w{3}$/;
+        exampleFormControlInput2.value = exampleFormControlInput2.value.toUpperCase();
     if (exampleFormControlInput2.value.match(RFCRegex) == null) {
-        alertError.style.display = "block";
+        alertError.style.display="block";
         alertError.innerHTML += "<br/>El RFC no es válido.";
-        }
+        exampleFormControlInput2.style.border = "solid red 1px";
+    }else {
+        exampleFormControlInput2.style.border = "solid green 1px";
+        validos++;
+        //exampleFormControlInput2.style.border = ""; //
+    }// else
+    if ((idTimeout=undefined) && (idTimeout=null)){
+        clearTimeout(idTimeout);
+    } // Timeout
 
-
+    if (validos == 3) {
+    idTimeout = setTimeout (function(){
+        exampleFormControlTextTarea1.style.border = "";
+        exampleFormControlInput1.style.border = "";
+        exampleFormControlInput2.style.border = "";
+    }, 3000);
+} // == 3
 });
